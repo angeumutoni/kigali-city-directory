5ccbcbd5dccd8582c8eaf7c4431de49903ad8eec
angeumutoni <a.umutoni1@alustudent.com>
2026-03-07 20:29:36 +0200

    Adding the map screen

---
+++ lib/screens/map_screen.dart
@@ -0,0 +1,16 @@
+```dart
+import 'package:flutter/material.dart';
+
+class MapScreen extends StatelessWidget {
+  const MapScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Map View (Google Maps will be added later)"),
+      ),
+    );
+  }
+}
+```
