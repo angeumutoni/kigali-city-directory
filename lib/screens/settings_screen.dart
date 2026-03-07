579d191713ae790bb2232fc1ff571c7268389a48
angeumutoni <a.umutoni1@alustudent.com>
2026-03-07 20:30:43 +0200

    Adding Settings Screen (Notifications Toggle)

---
+++ lib/screens/settings_screen.dart
@@ -0,0 +1,52 @@
+```dart
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/auth_provider.dart';
+
+class SettingsScreen extends StatefulWidget {
+  const SettingsScreen({super.key});
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+
+  bool notifications = true;
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(title: const Text("Settings")),
+
+      body: Column(
+        children: [
+
+          const ListTile(
+            title: Text("User"),
+            subtitle: Text("user@email.com"),
+          ),
+
+          SwitchListTile(
+            title: const Text("Location Notifications"),
+            value: notifications,
+            onChanged: (value) {
+              setState(() {
+                notifications = value;
+              });
+            },
+          ),
+
+          ElevatedButton(
+            onPressed: () {
+              context.read<AuthProvider>().logout();
+            },
+            child: const Text("Logout"),
+          )
+        ],
+      ),
+    );
+  }
+}
+```
